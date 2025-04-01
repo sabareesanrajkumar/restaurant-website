aa55b8749499e82bc84cd37b3d192bb855edb2b5
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-01 14:14:38 +0530

    header component

---
+++ restaurant-website/src/components/layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import mealsImage from '../../../assets/meals.jpeg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <h1>Eternal nectar</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className="main-image">
+        <img src={mealsImage} alt="restaurant" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ restaurant-website/src/components/layout/Header/HeaderCartButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderCartButton = () => {
+  return (
+    <>
+      <button>Cart</button>
+    </>
+  );
+};
+
+export default HeaderCartButton;
