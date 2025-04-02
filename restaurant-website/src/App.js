2bf232a584d96205e6fda5822d89cec9bc5033f7
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-02 10:27:18 +0530

    main

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/layout/Header/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meals />
     </div>
   );
 }
---
+++ restaurant-website/src/components/Meals/Meals.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Meals.css';
+
+const Meals = () => {
+  return (
+    <div className="summary">
+      <h2>Delicious Food,delivered to you</h2>
+      <p>
+        Choose your favourtie meal from our menu and enjoy you lunch or dinner
+        at your home!
+      </p>
+      <p>All our meal are catered with love!</p>
+    </div>
+  );
+};
+
+export default Meals;
