4ed287774da9a747777099a328be3af4cb21b569
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-02 10:51:13 +0530

    Menu visiblity

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Meals from './components/Meals/Meals';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Meals />
+      <Menu />
     </div>
   );
 }
---
+++ restaurant-website/src/components/Menu/Menu.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Menu.css';
+const Menu = () => {
+  const [items, setItems] = useState([
+    { id: 1, title: 'Briyani', price: 200 },
+    { id: 2, title: 'Fried rice', price: 250 },
+    { id: 1, title: 'Meals', price: 130 },
+  ]);
+  return (
+    <div className="menu">
+      {items.map((item) => {
+        return (
+          <div className="menu--item">
+            <h3>{item.title}</h3>
+            <h4>INR - {item.price}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
